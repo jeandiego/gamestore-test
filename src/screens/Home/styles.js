1dd0de499877fe845344d70fa46007d86fbd7f38
jeeanjames <jeandiego.ds@gmail.com>
2021-07-28 15:30:34 -0300

    ✨ Improved home screen structure

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Header = styled.View``;
+
+export const FiltersMenu = styled.View`
+  margin: 16px 0px;
+`;
+
+export const Title = styled.Text`
+  color: ${p => p.theme.colors.highlight};
+  font-size: 28px;
+  font-weight: bold;
+  padding: 0px 24px;
+`;
