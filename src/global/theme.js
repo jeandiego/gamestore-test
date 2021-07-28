41e58c7e367208d4a33ff70f4aba0e1db0e9275f
jeeanjames <jeandiego.ds@gmail.com>
2021-07-28 15:31:08 -0300

    :bento: Created new global styles

---
+++ src/global/theme.js
@@ -0,0 +1,16 @@
+const GlobalStyle = {
+  colors: {
+    primary: '#E46845',
+    secondary: '#ECF0F3',
+    background: '#FFFFFF',
+
+    overlay: 'rgba(0,0,0,0.7)',
+    orangelight: '#F9DBD2',
+    highlight: '#1B2438',
+    heading: '#7E8AA4',
+    off: '#D83D3A',
+    on: '#32BD50',
+  },
+};
+
+export default GlobalStyle;
