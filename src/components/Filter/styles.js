99130daaed37567d6ff7552ffd9e4f9ec3c1d1c7
jeeanjames <jeandiego.ds@gmail.com>
2021-07-28 19:08:35 -0300

    💄 Improvements on styles filter menu

--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -2,7 +2,7 @@ import {RectButton} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 import GlobalStyle from '../../global/theme';
 
-const {primary, secondary, background, highlight, orangelight, heading} =
+const {primary, background, highlight, orangelight, heading} =
   GlobalStyle.colors;
 
 export const Container = styled(RectButton)`
@@ -10,29 +10,18 @@ export const Container = styled(RectButton)`
 `;
 
 export const Content = styled.View`
-  width: 100%;
-  padding: 8px 16px;
+  padding: 6px 18px;
   background-color: ${p => (p.checked ? orangelight : background)};
   border-radius: 8px;
   justify-content: space-around;
   align-items: center;
-  border-color: ${p => (p.checked ? orangelight : heading)};
-  border-width: 1px;
-  border-radius: 12px;
-  opacity: ${p => (p.checked ? 1 : 0.8)};
+  border-color: ${p => p.theme.colors.orangelight};
+  border-width: 2px;
+  border-radius: 10px;
 `;
 
 export const Title = styled.Text`
   color: ${p => (p.checked ? primary : heading)};
   font-size: 14px;
   font-weight: ${p => (p.checked ? 'bold' : 'normal')};
-`;
-
-export const Check = styled.View`
-  background-color: ${p => p.theme.colors.highlight};
-  margin-right: 7px;
-`;
-
-export const Checked = styled(Check)`
-  background-color: ${p => p.theme.colors.primary};
 `;
