5175d0c2f74967aeb180bbd3534ac601c7940994
jeeanjames <jeandiego.ds@gmail.com>
2021-07-29 21:34:41 -0300

    ⚡️ Home screen improvements

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,24 @@
 import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
 import {HomeView} from './view';
 import {getProducts} from '../../controller/products';
+import {filterIds} from '../../utils/filters';
+import {
+  setFilter,
+  setDefaultProducts,
+  setProducts,
+} from '../../store/reducers/market';
+import {sortAlphabetically} from '../../utils/sort';
 
 export function Home() {
-  const [filter, setFilter] = useState('');
-  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const {
+    cart: {quantity},
+    market: {defaultProducts, products, filter},
+  } = useSelector(store => store);
+  const dispatch = useDispatch();
+
   const navigation = useNavigation();
 
   function handleHomeClick() {
@@ -13,23 +26,52 @@ export function Home() {
   }
 
   function handleFilterSelect(filterId) {
-    return filterId === filter ? setFilter('') : setFilter(filterId);
-  }
-
-  async function fetchProducts() {
-    const productsList = await getProducts();
-
-    setProducts(productsList);
+    return filterId === filter
+      ? dispatch(setFilter(0))
+      : dispatch(setFilter(filterId));
   }
 
   useEffect(() => {
+    async function fetchProducts() {
+      setLoading(true);
+      const productsList = await getProducts();
+
+      dispatch(setProducts(productsList));
+      dispatch(setDefaultProducts(productsList));
+      setLoading(false);
+    }
     fetchProducts();
-  }, []);
+  }, [dispatch]);
+
+  useEffect(() => {
+    function handleFilter(_products) {
+      switch (filter) {
+        case filterIds.popular:
+          return _products.sort((a, b) => b.score - a.score);
+        case filterIds.lowPrice:
+          return _products.sort((a, b) => a.price - b.price);
+        case filterIds.highPrice:
+          return _products.sort((a, b) => b.price - a.price);
+        case filterIds.aZ:
+          return _products.sort((a, b) => sortAlphabetically(a, b));
+        case filterIds.zA:
+          return _products.sort((a, b) => sortAlphabetically(b, a));
+        default:
+          return defaultProducts;
+      }
+    }
+    if (defaultProducts) {
+      const ordernedProducts = handleFilter([...defaultProducts]);
+      dispatch(setProducts(ordernedProducts));
+    }
+  }, [dispatch, filter, defaultProducts]);
 
   return (
     <HomeView
       filter={filter}
       products={products}
+      loading={loading}
+      quantity={quantity}
       handleHomeClick={handleHomeClick}
       handleFilterSelect={handleFilterSelect}
     />
